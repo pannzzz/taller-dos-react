48ba51c7d00fec7f743d11c28c7135bf45f73e88
Panzzz23 <maria.jimenez544@soy.sena.edu.co>
2024-09-17 22:31:33 -0500

    ejercicio 1 Maria Jose Jimenez Diaz
    se soluciono el punto uno presentando el saludo a majo jijiji

---
+++ ejercicio1/src/Saludo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Saludo = ({ nombre }) => {
+    return (
+        <div>
+            <h1>Hola, { nombre }! uwu </h1>
+        </div>
+    );
+};
+
+export default Saludo;
